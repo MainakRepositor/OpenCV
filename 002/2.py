f757527892141efcc2faba1b339f997753e50b2a
MainakRepositor <sultankhilji001@gmail.com>
2021-06-06 16:02:18 +0530

    basic commit ⭐

---
+++ 002/2.py
@@ -0,0 +1,11 @@
+import cv2
+
+src = cv2.imread("test.jpg")
+cv2.namedWindow("input", cv2.WINDOW_AUTOSIZE)
+cv2.imshow("input", src)
+gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+cv2.imwrite('gray.png', gray)
+cv2.imshow("gray", gray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
